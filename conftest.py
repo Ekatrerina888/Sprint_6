d6b23fe86643ab414644f2e1e039c8a67968951c
Ekaterina Bakhshieva <ekaterina_bakhshieva@mail.ru>
2026-06-11 02:21:16 +0400

    Добавлены тесты, локаторы, отчёт

--- conftest.py
+++ conftest.py
@@ -4,7 +4,7 @@ from data import Urls
 
 @pytest.fixture(scope="function")
 def driver():
-    driver = webdriver.Chrome()
+    driver = webdriver.Firefox()
     driver.get(Urls.MAIN_PAGE)
     driver.maximize_window()
     yield driver
